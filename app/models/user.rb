670ed80f3ffff6289a200e1f947802437dc2d4e0
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-06-10 19:28:39 +0900

    Fixed according to comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
+  extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
   enum role: [:user, :admin]
-  after_initialize :set_default_role, :if =>  new_record?
-
-  def set_default_role
-    self.role ||= :user
+  after_initialize do
+    if self.new_record?
+      self.role ||= :user
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only =>[:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
