0756ddb3f16cd418c2f8b183c956813fd971bdbd
IstiakAhmed-1721365 <s.m.ahmed@northsouth.edu>
2022-06-13 19:25:11 +0900

    fixed google login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -33,7 +33,8 @@ module Users
     end
 
     def handle_auth(kind)
-      @user = User.from_omniauth(request.env[omniauth.auth])
+      
+      @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind:kind
         sign_in_and_redirect @user, event: :authentication
