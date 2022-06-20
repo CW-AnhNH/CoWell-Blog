96acb8151ee652df275a70ce48a03f0cd8e7cb15
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-06-20 20:36:13 +0900

    create and update user

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get 'users/index'
     # routes for admin
   end
-  root 'pages#home'
+  root 'posts#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
