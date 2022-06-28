7149d73d7167fef009e48db65ed1e5aca50e8114
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-06-28 17:40:16 +0900

    fixed routing for groups

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
   resources :posts
   resources :users
+  resources :groups
   root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
