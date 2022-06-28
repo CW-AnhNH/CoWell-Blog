b489126ed2f07e1cdeb2a9fee26524477968b55e
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-06-28 11:47:12 +0900

    fixed unnecessary comma

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // import 'bootstrap'
 import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false`
+Turbo.session.drive = false
 import "jquery"
 import "controllers"
 import "./custom/preview_image"
