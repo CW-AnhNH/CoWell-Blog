c56c2f4c03016b252250907278e7830acd223aa2
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-07-05 11:16:30 +0900

    fixing schema merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_04_082043) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_24_082345) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,10 +50,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_082043) do
   create_table "groups", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "privacy"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "privacy", default: 0
   end
 
   create_table "post_votings", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -67,12 +67,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_082043) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "status"
     t.integer "user_id"
     t.integer "group_id"
-    t.integer "privacy"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
+    t.integer "privacy", default: 0
   end
 
   create_table "relationships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -95,6 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_082043) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "status", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
