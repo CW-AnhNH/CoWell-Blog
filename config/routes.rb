b8cac96dedabc86d5e4bdb6ec20ccc2b9c1e7703
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-07-05 11:01:33 +0900

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   resources :groups
-  root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
