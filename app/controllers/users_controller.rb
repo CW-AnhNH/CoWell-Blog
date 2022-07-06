6ee939c6f1b20631f46464c89ab7ce103634da55
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-07-06 15:54:55 +0900

    implemented following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,12 @@ class UsersController < ApplicationController
   end
 
   def follow
-    NotificationMailer.followed_email(@user).deliver
-    current_user.followees << @user
+    current_user.followeds << @user
     redirect_to user_path(@user)
   end
 
   def unfollow
-    NotificationMailer.unfollowed_email(@user).deliver
-    current_user.followed_users.find_by(followee_id: @user.id).destroy
+    current_user.followed_users.find_by(followed_id: @user.id).destroy
     redirect_to user_path(@user)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   resources :groups
+
+  post '/users/:id/follow', to: "users#follow", as: "follow_user"
+  post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
+  
   root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
